77a29585cc06405b457bc054cc74ba0cd599a5af
Jerred Shepherd <ShepherdJerred@gmail.com>
2017-11-02 17:29:44 -0500

    Add old WPF code

---
+++ lights-out-uwp/LightsOutGame.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace lights_out_uwp {
+    internal class LightsOutGame {
+        private bool[,] _grid; // Stores on/off state of cells in g
+        private int _numCells = 3; // Number of cells in grid
+        private readonly Random _rand; // Used to generate random numbers
+
+        public LightsOutGame() {
+            _rand = new Random(); // Initialize random number generator
+            _grid = new bool[_numCells, _numCells];
+            // Turn entire grid on
+            for (var r = 0; r < _numCells; r++) {
+                for (var c = 0; c < _numCells; c++) {
+                    _grid[r, c] = true;
+                }
+            }
+        }
+
+        // Returns the number of horizontal/vertical cells in the grid
+        public int NumCells {
+            get => _numCells;
+            set {
+                _numCells = value;
+            }
+        }
+
+        // Returns the grid value at the given row and column
+        public bool GetGridValue(int row, int col) {
+            return _grid[row, col];
+        }
+
+        // Randomizes the grid
+        public void NewGame() {
+            _grid = new bool[_numCells, _numCells];
+            // Fill grid with either white or black
+            for (var r = 0; r < _numCells; r++) {
+                for (var c = 0; c < _numCells; c++) {
+                    _grid[r, c] = _rand.Next(2) == 1;
+                }
+            }
+        }
+
+        // Inverts the selected box and all surrounding boxes
+        public void MakeMove(int row, int col) {
+            for (var i = row - 1; i <= row + 1; i++) {
+                for (var j = col - 1; j <= col + 1; j++) {
+                    if (i >= 0 && i < _numCells && j >= 0 && j < _numCells) {
+                        _grid[i, j] = !_grid[i, j];
+                    }
+                }
+            }
+        }
+
+        // Returns true if all cells are off
+        public bool PlayerWon() {
+            for (var r = 0; r < _numCells; r++) {
+                for (var c = 0; c < _numCells; c++) {
+                    if (_grid[r, c]) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
--- lights-out-uwp/MainPage.xaml.cs
+++ lights-out-uwp/MainPage.xaml.cs
@@ -1,30 +1,91 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+using Windows.UI.Popups;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+using Windows.UI.Xaml.Shapes;
 
 namespace lights_out_uwp
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
-    {
+    public sealed partial class MainPage {
+        private LightsOutGame _game;
+
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+
+            _game = new LightsOutGame();
+            InitGame();
+        }
+
+        private void InitGame() {
+            _game.NewGame();
+            CreateGrid();
+        }
+
+        private void CreateGrid() {
+            int rectSize = (int)CanvasMain.Width / _game.NumCells;
+            CanvasMain.Children.Clear();
+            // Create rectangles for grid
+            for (int r = 0; r < _game.NumCells; r++) {
+                for (int c = 0; c < _game.NumCells; c++) {
+                    Rectangle rect = new Rectangle();
+                    rect.Fill = new SolidColorBrush(Windows.UI.Colors.White);
+                    rect.Width = 20;
+                    rect.Height = 20;
+                    rect.Stroke = new SolidColorBrush(Windows.UI.Colors.Black);
+                    // Store each row and col as a Point
+                    rect.Tag = new Point(r, c);
+                    // Register event handler
+                    rect.PointerPressed += Rect_PointerPressed;
+                    // Put the rectangle at the proper location within the canvas
+                    Canvas.SetTop(rect, r * rectSize);
+                    Canvas.SetLeft(rect, c * rectSize);
+                    // Add the new rectangle to the canvas' children
+                    CanvasMain.Children.Add(rect);
+                }
+            }
+//            UpdateGridColors();
+        }
+
+        private void Rect_PointerPressed(object sender, PointerRoutedEventArgs e) {
+            Rectangle rect = sender as Rectangle;
+            if (rect != null) {
+                var rowCol = (Point)rect.Tag;
+                int row = (int)rowCol.X;
+                int col = (int)rowCol.Y;
+                _game.MakeMove(row, col);
+            }
+            UpdateGridColors();
+            CheckWinCondition();
+        }
+
+        private async void CheckWinCondition() {
+            if (_game.PlayerWon()) {
+                MessageDialog msgDialog = new MessageDialog("Congratulations! You've won!", "Lights Out!");
+                msgDialog.Commands.Add(new UICommand("OK"));
+                await msgDialog.ShowAsync();
+            }
+        }
+
+        private void UpdateGridColors() {
+            int index = 0;
+
+            // Set the colors of the rectangles
+            for (int r = 0; r < _game.NumCells; r++) {
+                for (int c = 0; c < _game.NumCells; c++) {
+                    Rectangle rect = CanvasMain.Children[index] as Rectangle;
+                    index++;
+                    if (_game.GetGridValue(r, c)) {
+                        rect.Fill = new SolidColorBrush(Windows.UI.Colors.White);
+                        rect.Stroke = new SolidColorBrush(Windows.UI.Colors.Black);
+                    } else {
+                        rect.Fill = new SolidColorBrush(Windows.UI.Colors.Black);
+                        rect.Stroke = new SolidColorBrush(Windows.UI.Colors.White);
+                    }
+                }
+            }
         }
     }
 }
